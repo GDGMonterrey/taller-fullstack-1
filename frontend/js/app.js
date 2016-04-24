e65b7f7d537e029248333aa847e1bc418c0a0360
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-04-23 19:01:12 -0500

    Added http request to api

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,25 @@
 var app = angular.module('pokeApp',[]);
 
-app.controller('HomeController', function() {
-  this.message = "Vamo a calmaro";
-});
+app.controller('HomeController', ['$http', function($http) {
+  var _this = this;
+  _this.message = "Vamo a calmaro";
+  $http.get("api/pokemon").then(function(response) {
+    _this.pokemons = response.data;
+   }, function(error) {
+    _this.pokemons = []
+  });
+}]);
+
+app.factory('Pokeio', ['$http', '$q', function($http, $q) {
+  var service = {};
+  service.get = function(url) {
+    var defer = $q.defer();
+    $http.get("api/" + url).then(function(response) {
+      return defer.resolve(response.data);
+     }, function(error) {
+      return defer.reject(error);
+    });
+    return defer.promise;
+  };
+  return service;
+}]);
